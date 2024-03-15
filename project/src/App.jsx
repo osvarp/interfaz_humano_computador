ddb9b71fa838713ed0ac97b43808605abf85d10b
fteran04 <110037380+fteran04@users.noreply.github.com>
2024-03-15 11:34:28 -0500

    commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,5 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/root.jsx";
 import Login from "./pages/login.jsx"
 import Register from "./pages/register.jsx";
 import Explore from "./pages/explore.jsx";
