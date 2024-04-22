5afd90954c689ee94a368f090e4109123f1c8186
Osvarp <osvaargas@gmail.com>
2024-04-21 22:53:51 -0500

    conflict resolved

--- project/src/components/displayProduct.jsx
+++ project/src/components/displayProduct.jsx
@@ -5,23 +5,7 @@ function DisplayProduct( { product } ) {
     const sellerUser = useSelector( (state) => state.allUsers[product.userName] );
 
     return (
-        /*
-        <div className={styles.productBox} >
-            <div className = {styles.imageFlexBox} >
-                <img src={sellerUser.profileImage} className={styles.profileImage} />
-                <h4 className={styles.productDisplayText}> {product.userName} </h4>
-            </div>
 
-            <div className={styles.imageFlexBox}>
-                <img src={product.productImage} className={styles.productImage} />
-                <div className={styles.priceBlock} >
-                    <h2 className={styles.productDisplayText}> {product.productName} </h2>
-                    <h4 className={styles.productDisplayText}> Price: ${product.price} </h4>
-                </div>
-            </div>
-
-            <p className={styles.productDisplayText}> { product.productDescription } </p>
-        </div>*/
         <div className={styles.userInfo}>
             <div className = {styles.imageFlexBox} >
                 <img src={sellerUser.profileImage} className={styles.profileImage} />
